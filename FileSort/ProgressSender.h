3253ff7229c3f83a8a3070522b1e030c626f8bb5
tvz <tvz.041@gmail.com>
2025-03-15 11:36:27 +0700

    Добавлено описание задания

--- FileSort/ProgressSender.h
+++ FileSort/ProgressSender.h
@@ -8,7 +8,16 @@ class ProgressSender : public QObject
 public:
     explicit ProgressSender(QObject *parent = nullptr);
 
+    /**
+     * @brief Сообщает GUI начальное количество отрезков.
+     * @param count количество отрезков.
+     */
     void setSegmentCount(int count);
+
+    /**
+     * @brief Сообщает GUI об уменьшении числа отрезков.
+     * @param segmentLeft количество оставшихся отрезков.
+     */
     void onSegmentMerged(int segmentLeft);
 
 signals:
